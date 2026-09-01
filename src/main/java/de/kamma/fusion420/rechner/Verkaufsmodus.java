6c73d8c257b8f6b93fc5804537dbce2c607e2a91
Lars Anton Rossbach <l.anton.rossbach@gmail.com>
2026-09-02 00:43:20 +0200

    Verkaufsoptionen fuer Sofort und Order

---
+++ src/main/java/de/kamma/fusion420/rechner/Verkaufsmodus.java
@@ -0,0 +1,41 @@
+package de.kamma.fusion420.rechner;
+
+import de.kamma.fusion420.markt.Preis;
+
+/** Preisbasis fuer den Verkauf der erzeugten Shards. */
+public enum Verkaufsmodus {
+	SOFORT("Sofort", "Sofort verkaufen"),
+	ORDER("Order", "Verkaufsangebot");
+
+	private final String anzeige;
+	private final String detail;
+
+	Verkaufsmodus(String anzeige, String detail) {
+		this.anzeige = anzeige;
+		this.detail = detail;
+	}
+
+	public String anzeige() { return anzeige; }
+	public String detail() { return detail; }
+
+	public double preis(Preis p) {
+		return switch (this) {
+			case SOFORT -> p.sofortVerkaufPreis();
+			case ORDER -> p.sofortKaufPreis();
+		};
+	}
+
+	public Verkaufsmodus naechster() {
+		Verkaufsmodus[] alle = values();
+		return alle[(ordinal() + 1) % alle.length];
+	}
+
+	public static Verkaufsmodus ausText(String text) {
+		if (text != null) {
+			for (Verkaufsmodus m : values()) {
+				if (m.name().equalsIgnoreCase(text)) return m;
+			}
+		}
+		return SOFORT;
+	}
+}
